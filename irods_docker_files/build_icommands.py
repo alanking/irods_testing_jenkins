b19ee0e10e12d04eae9eb125eb818d56c935801f
Alan King <alanking@renci.org>
2020-04-13 10:47:10 -0400

    Build with externals

--- irods_docker_files/build_icommands.py
+++ irods_docker_files/build_icommands.py
@@ -7,15 +7,17 @@ import os
 import configuration
 
 def build_packages(
-    input_repo_directory,
-    input_irods_directory,
     platform_target,
     build_id,
-    output_directory):
+    input_repo_directory,
+    input_irods_directory,
+    output_directory,
+    externals_directory=None):
 
     base_os_image_tag = ':'.join([configuration.base_os_image_name, platform_target])
     builder_container_name = '-'.join([platform_target, 'build_icommands', build_id])
     input_repo_directory_bind = '/code_to_build'
+    externals_directory_bind = '/externals_to_use_with_build'
     input_irods_directory_bind = '/built_irods_packages_to_install'
     output_directory_bind = '/built_packages_output'
     build_hook_path = os.path.join(input_repo_directory_bind, configuration.build_hook_filename)
@@ -34,16 +36,24 @@ def build_packages(
         }
     }
 
+    if externals_directory:
+        volumes[externals_directory] = {
+            'bind': externals_directory_bind,
+            'mode': 'ro'
+        }
+
     # run build hook
     client = docker.from_env()
     cmd = [
         'python', build_hook_path,
         '--irods_packages_root_directory', input_irods_directory_bind,
+        '--externals_packages_directory', externals_directory_bind,
         '--output_root_directory', output_directory_bind,
         '--verbose'
     ]
     print('building icommands from code in [{0}] on host machine (mounted in container as:[{1}])'.format(input_repo_directory, input_repo_directory_bind))
     print('installing irods-dev and irods-runtime from [{0}] on host machine (mounted in container as:[{1}])'.format(input_irods_directory, input_irods_directory_bind))
+    print('using externals in [{0}] on host machine (mounted in container at: [{1}])'.format(externals_directory, externals_directory_bind))
     print('placing packages in [{0}] on host machine (mounted in container at: [{1}])'.format(output_directory, output_directory_bind))
     return client.containers.run(
         image=base_os_image_tag,
@@ -57,6 +67,7 @@ def main():
     parser.add_argument('-p', '--platform_target', type=str, required=True)
     parser.add_argument('-b', '--build_id', type=str, required=True)
     parser.add_argument('-o', '--output_directory', type=str, required=True)
+    parser.add_argument('-e', '--externals_packages_directory', type=str, default=None)
     parser.add_argument('--repo_directory_identifier', type=str, required=True)
     parser.add_argument('--irods_packages_root_directory', type=str, required=True)
     args = parser.parse_args()
@@ -69,11 +80,12 @@ def main():
         args.irods_packages_root_directory,
         configuration.platform_packages_dir_map[args.platform_target])
     result_output = build_packages(
-        input_repo_directory,
-        input_irods_directory,
         args.platform_target,
         args.build_id,
-        args.output_directory)
+        input_repo_directory,
+        input_irods_directory,
+        args.output_directory,
+        args.externals_packages_directory)
     print(result_output)
     print('build completed successfully')
 
--- irods_docker_files/build_irods_on_base_os_image.py
+++ irods_docker_files/build_irods_on_base_os_image.py
@@ -7,14 +7,16 @@ import os
 import configuration
 
 def build_irods_packages(
-    input_directory,
     platform_target,
     build_id,
-    output_directory):
+    input_directory,
+    output_directory,
+    externals_directory=None):
 
     base_os_image_tag = ':'.join([configuration.base_os_image_name, platform_target])
     irods_builder_container_name = '-'.join([platform_target, 'build_irods', build_id])
     input_directory_bind = '/irods_code_to_build'
+    externals_directory_bind = '/externals_to_use_with_build'
     output_directory_bind = '/built_packages_output'
     build_hook_path = os.path.join(input_directory_bind, configuration.build_hook_filename)
     volumes = {
@@ -28,14 +30,22 @@ def build_irods_packages(
         }
     }
 
+    if externals_directory:
+        volumes[externals_directory] = {
+            'bind': externals_directory_bind,
+            'mode': 'ro'
+        }
+
     # run build hook
     client = docker.from_env()
     cmd = [
         'python', build_hook_path,
         '--output_root_directory', output_directory_bind,
+        '--externals_packages_directory', externals_directory_bind,
         '--verbose'
     ]
     print('building irods from code in [{0}] on host machine (mounted in container as:[{1}])'.format(input_directory, input_directory_bind))
+    print('using externals in [{0}] on host machine (mounted in container at: [{1}])'.format(externals_directory, externals_directory_bind))
     print('placing packages in [{0}] on host machine (mounted in container at: [{1}])'.format(output_directory, output_directory_bind))
     return client.containers.run(
         image=base_os_image_tag,
@@ -49,6 +59,7 @@ def main():
     parser.add_argument('-p', '--platform_target', type=str, required=True)
     parser.add_argument('-b', '--build_id', type=str, required=True)
     parser.add_argument('-o', '--output_directory', type=str, required=True)
+    parser.add_argument('-e', '--externals_packages_directory', type=str, default=None)
     parser.add_argument('--repo_directory_identifier', type=str, required=True)
     args = parser.parse_args()
 
@@ -58,10 +69,11 @@ def main():
         args.repo_directory_identifier,
         args.build_id)
     result_output = build_irods_packages(
-        input_directory,
         args.platform_target,
         args.build_id,
-        args.output_directory)
+        input_directory,
+        args.output_directory,
+        args.externals_packages_directory)
     print(result_output)
     print('build completed successfully')
 
