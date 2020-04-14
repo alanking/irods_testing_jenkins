763758abc6b40ba542efb19f42373288c1855391
Alan King <alanking@renci.org>
2020-04-14 15:43:09 -0400

    clone repo: renaming parameters, strong defaults

--- irods_docker_files/clone_repo.py
+++ irods_docker_files/clone_repo.py
@@ -5,29 +5,36 @@ import git
 import os
 import shutil
 import sys
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
 
 parser = argparse.ArgumentParser(description='Clone Git Repository')
-parser.add_argument('-b', '--build_id', type=str, required=True)
+parser.add_argument('--unique_id', type=str, required=True)
 parser.add_argument('--repo', type=str, required=True)
 parser.add_argument('--commitish', type=str, required=True)
-parser.add_argument('--repo_directory_identifier', type=str, required=True)
+parser.add_argument('--clone_target_directory', type=str)
 parser.add_argument('--ignore_cache', action='store_true', default=False)
 args = parser.parse_args()
 
-# TODO: Turn '/jenkins_output' into environment variable or configuration value
-# Full path to destination within the "wormhole" mountpoint inside the Jenkins container
-repo_directory = os.path.join('/jenkins_output', args.repo_directory_identifier, args.build_id)
-if os.path.exists(repo_directory):
+target_directory = args.clone_target_directory
+if not target_directory:
+    target_directory = os.path.join(
+        '/jenkins_output',
+        os.path.basename(urlparse(args.repo).path),
+        args.unique_id)
+
+if os.path.exists(target_directory):
     if not args.ignore_cache:
-        print('using previously cloned repository at ' + repo_directory)
+        print('non-empty directory found at [{0}] - skipping clone'.format(target_directory))
         sys.exit(0)
+    shutil.rmtree(target_directory)
 
-    shutil.rmtree(repo_directory)
-
-print('cloning repository from [{0}]@[{1}] to [{2}]'.format(args.repo, args.commitish, repo_directory))
+print('cloning repository from [{0}]@[{1}] to [{2}]'.format(args.repo, args.commitish, target_directory))
 repo = git.Repo.clone_from(
     args.repo,
-    repo_directory,
+    target_directory,
     branch=args.commitish)
 repo.submodule_update()
 print('cloning completed successfully')
